3dedf8b09dd87246ce5afc674f597382d5c9898f
Santosh Srivatsan <44707773+SantoshSrivatsan24@users.noreply.github.com>
2022-08-29 16:06:21 -0400

    Removed Pictures/

--- .config/spicetify/Extracted/Raw/xpui/xpui-routes-create-playlist.js
+++ .config/spicetify/Extracted/Raw/xpui/xpui-routes-create-playlist.js
@@ -1,2 +1,2 @@
-"use strict";(("undefined"!=typeof self?self:global).webpackChunkopen=("undefined"!=typeof self?self:global).webpackChunkopen||[]).push([[985],{6349:(e,l,n)=>{n.r(l),n.d(l,{CreatePlaylist:()=>a,default:()=>s});var t=n(67294),f=n.n(t),u=n(63282),a=f().memo((function(){var e=(0,u.K)().createPlaylist;return(0,t.useEffect)((function(){e()}),[e]),null}));const s=a}}]);
+"use strict";(("undefined"!=typeof self?self:global).webpackChunkopen=("undefined"!=typeof self?self:global).webpackChunkopen||[]).push([[7985],{32267:(e,l,n)=>{n.r(l),n.d(l,{default:()=>u});var t=n(67294),f=n(63282);const s=t.memo((function(){const{createPlaylist:e}=(0,f.K)();return(0,t.useEffect)((()=>{e()}),[e]),null})),u=s}}]);
 //# sourceMappingURL=xpui-routes-create-playlist.js.map
